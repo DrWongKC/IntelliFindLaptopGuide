f9fc5c175962dcbfda1fc294f9349d24b4942cf6
Wong Kai Chong 黄恺聪 <KAI_Wong@outlook.com>
2026-09-02 15:17:32 +0800

    added list function that will help with communicating to an AI of what the user wants | still under progress

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [specifications, setSpecifications] = useState([]);
+
+  const specificationsListItems = specifications.map((specification, index) => (
+    <li key={index}>{specification}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newSpecification = formData.get("specification");
+    setSpecifications((prevSpecifications) => [
+      ...prevSpecifications,
+      newSpecification,
+    ]);
+  }
+
+  console.log(specificationsListItems);
+
   return (
     <main>
       <h2>Welcome to IntelliFindLaptopGuide</h2>
       <p>
         Please add only 1 specification at a time.
         <br />
-        (e.g. 16GB RAM or 1TB SSD or Intel Ultra7)
+        (e.g. 16GB RAM or 1TB SSD or Intel Ultra5)
       </p>
-      <form className="add-specification-form">
+      <form onSubmit={handleSubmit} className="add-specification-form">
         <input
           type="text"
+          name="specification"
           placeholder="e.g. 32GB RAM"
           aria-label="Add a laptop's specification"
         />
         <button>Add specification</button>
       </form>
+      <ul>{specificationsListItems}</ul>
     </main>
   );
 }
